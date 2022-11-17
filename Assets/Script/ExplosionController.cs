b7260319266b7a691a79ee37be295a1f3f942385
Shinochi-Haru <haru060109011305@gmail.com>
2022-11-17 22:07:50 +0900

    [fix]爆発改善

--- Assets/Script/ExplosionController.cs
+++ Assets/Script/ExplosionController.cs
@@ -3,32 +3,47 @@ using UnityEngine;
 public class ExplosionController : MonoBehaviour
 {
     [SerializeField] ParticleSystem m_particle;
-    [SerializeField] float m_force = 20;
-    [SerializeField] float m_radius = 5;
-    [SerializeField] float m_upwards = 0;
-    Vector3 m_position;
+    [SerializeField] float _force = 20;
+    [SerializeField] float _radius = 5;
+    [SerializeField] float _upwards = 0;
+    Vector3 _position;
+    Rigidbody rb;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Explosion();
+            Destroy(gameObject);
         }
     }
+    /// <summary>
+    /// ”j‰ó‚µ‚½Œã‚É”š”­‚·‚é
+    /// </summary>
+    private void OnDestroy()
+    {
+        Explosion();
+    }
 
+    /// <summary>
+    /// ”š”­‚·‚éŒø‰Ê
+    /// </summary>
     public void Explosion()
     {
         //m_particle.Play();
-       // m_position = m_particle.transform.position;
+        // m_position = m_particle.transform.position;
 
         // ”ÍˆÍ“à‚ÌRigidbody‚ÉAddExplosionForce
-        Collider[] hitColliders = Physics.OverlapSphere(m_position, m_radius);
-        for (int i = 0; i < hitColliders.Length; i++)
+        Vector3 _explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(_explosionPos, _radius);
+        //Collider[] hitColliders = Physics.OverlapSphere(m_position, m_radius);
+        //for (int i = 0; i < colliders.Length; i++)
+        foreach (Collider hit in colliders)
         {
-            var rb = hitColliders[i].GetComponent<Rigidbody>();
-            if (rb)
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            //var rb = colliders[i].GetComponent<Rigidbody>();
+            if (rb != null)
             {
-                rb.AddExplosionForce(m_force, m_position, m_radius, m_upwards, ForceMode.Impulse);
+                rb.AddExplosionForce(_force, _explosionPos, _radius, _upwards, ForceMode.Impulse);
             }
         }
     }
